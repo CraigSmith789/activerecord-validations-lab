88dbb6d643c074222fd655997ad20c741f09ae9c
CraigSmith789 <craig789@gmail.com>
2021-01-05 12:50:08 -0500

    commit tree 85d6655d3549b6ab11c2aa0baf8cc76fb44dcdfa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  
+  validates :title, presence: true
+  validates :content, length: { minimum:250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+  private
+  
+  def title_rules_adhered?
+    if title != nil
+    title.include?("Won't Believe" || "Secret Top [number]" || "Guess")
+    end
+  end
+  def clickbait?
+    unless title_rules_adhered?
+      errors.add(:title, "must contain 'clickbait'")
+    end  
+  end
+
 end
